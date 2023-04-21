3b06b3c68a18b1513cefab66fcb67d62bdb1ee4e
Shin <shin62mahilum@gmail.com>
2023-04-21 08:33:23 +0800

    Thesis Project Mobile App V2

---
+++ app/src/main/java/com/example/thesisprojectmobileapp/ReserveActivity.java
@@ -0,0 +1,21 @@
+package com.example.thesisprojectmobileapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import java.util.Calendar;
+
+public class ReserveActivity<Calendar> extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reserve);
+
+
+    }
+}
